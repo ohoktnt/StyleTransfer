f70c574f4fad122a015a2157ddade8c14cb9d15a
Tammy <tammy.n.tran@gmail.com>
2021-05-05 12:57:00 -0400

    complete styling for UI

--- App.js
+++ App.js
@@ -6,19 +6,64 @@ import ProgressBar from 'react-native-progress/Bar';
 export default class App extends Component {
   render() {
     return (
-      <View>
-        <View>
-          <Text>Neural Style Transfer</Text>
-          <Text>Art Style Transfer Generator</Text>
+      <View style={styles.container}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Neural Style Transfer</Text>
+          <Text style={styles.subtitle}>Art Style Transfer Generator</Text>
         </View>
-        <View>
-          <Image></Image>
+        <View style={styles.imageContainer}>
+          <Image
+            source={require('./assets/painting1.png')}
+            style={styles.artImage}></Image>
         </View>
-        <View>
-          <Button title="Select Base Image" ></Button>
-          <Button title="Select Style Image"></Button>
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Select Base Image"
+            buttonStyle={styles.button}></Button>
+          <Button
+            title="Select Style Image"
+            buttonStyle={styles.button}></Button>
         </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#6adfe6',
+    flex: 1,
+  },
+  titleContainer: {
+    marginTop: 80,
+    marginLeft: 30,
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  subtitle: {
+    fontSize: 17,
+  },
+  imageContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    paddingBottom: 40,
+    alignItems: 'center',
+  },
+  button: {
+    width: 200,
+    height: 57,
+    backgroundColor: 'black',
+    borderRadius: 8,
+    margin: 5,
+  },
+  artImage: {
+    width: 260,
+    height: 260,
+  },
+});
